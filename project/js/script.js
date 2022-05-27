1ab6c38e95825477924381665e127ea2370f18c7
NikolasSnorkell <pushok_nik@mail.ru>
2022-05-27 17:19:34 +0300

    Started project. Made header

---
+++ project/js/script.js
@@ -0,0 +1,9 @@
+
+
+$(".burger-btn").click(function(){
+    $('#burger-menu').animate({"right":"0"},400);
+})
+$(".burger-nav_back_btn").click(function(){
+    $('#burger-menu').animate({"right":"-100%"},400);
+})
+
